85457305a84ded6d6b058c65e714b27754ffcf67
Jason Boerner <jboerner56@gamil.com>
2019-04-30 15:05:07 -0400

    files for routes

---
+++ src/About.js
@@ -0,0 +1,9 @@
+import React from 'react';
+    function About (props) {
+        return(
+            <div>
+                <h1>Home</h1>
+            </div>
+        )
+    }
+export default About;
---
+++ src/Dogs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+    function Dogs (props) {
+        return(
+            <div>
+                <h1>Home</h1>
+            </div>
+        )
+    }
+export default Dogs;
